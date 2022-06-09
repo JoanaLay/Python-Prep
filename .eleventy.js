b22af89abb8bd334916cebe7e18e9ea4aaf29221
Joanalay <joanalay87@gmail.com>
2022-06-09 19:41:28 -0300

    joe

--- .eleventy.js
+++ .eleventy.js
@@ -24,7 +24,7 @@ const mdAnchorOpts = {
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.setLibrary(
-    'md',
+    'pd',
     markdownIt(mdOptions)
       .use(markdownItAnchor, mdAnchorOpts)
       .use(markdownItHighlightJS)
